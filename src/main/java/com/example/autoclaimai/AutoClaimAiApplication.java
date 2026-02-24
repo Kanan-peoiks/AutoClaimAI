31147efe3ef0b4e99314be712575ac47e9abc57f
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-02-24 15:54:56 +0400

    Initial commit

---
+++ src/main/java/com/example/autoclaimai/AutoClaimAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.autoclaimai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoClaimAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoClaimAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/autoclaimai/AutoClaimAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autoclaimai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoClaimAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
