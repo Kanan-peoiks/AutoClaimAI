45f19f9de2b7b1781ff0783e0cbfb5a386151a72
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-02-26 04:08:45 +0400

    feat: add controller package

---
+++ IdentityService/src/main/java/com/example/identityservice/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.example.identityservice.controller;
+
+
+import com.example.identityservice.dto.AuthRequest;
+import com.example.identityservice.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public String register(@RequestBody AuthRequest request) {
+        return authService.register(request);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody AuthRequest request) {
+        return authService.login(request);
+    }
+}
